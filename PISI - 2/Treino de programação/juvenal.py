c5e00ce1528d04a9986a1e55fb4bb912280f0546
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2022-12-04 10:15:21 -0300

    Create juvenal.py

---
+++ PISI - 2/Treino de programação/juvenal.py
@@ -0,0 +1,40 @@
+def f(n, stepcount):
+    stepcount += 1
+    if n > 1:
+        if n % 2 == 0:
+            return f(n/2, stepcount)
+        else:
+            return f(3*n+1, stepcount)
+    return stepcount
+
+def g(a, b):
+    '''
+    Função para calcular quantas chamadas recursivas são necessárias para que F(n) atinja o caso base
+    '''
+    bigint = 0
+    # Verificando o maior valor quando 'n' está no intervalo [A, B]
+    while a <= b:
+        steps = f(a, 0)
+        if steps > bigint:
+            bigint = steps
+        a += 1
+    return bigint
+
+# Número de casos de teste
+while True:
+    t = int(input("Número de casos de teste: "))
+    if 1 <= t <= 100:
+        break
+
+maiores_valores = []
+# Inteiros A e B
+for _ in range(t):
+    while True:
+        a, b = [int(i) for i in input().split()]
+        if 1 <= a <= b <= 100000:
+            maior_valor = g(a, b)
+            maiores_valores.append(maior_valor)
+            break
+
+for i, v in enumerate(maiores_valores):
+    print(f"Caso {i+1}: {v}")
